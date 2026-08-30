67f62094701ef2f159ac8ef034409bf690c07bf8
Karssou <alexandrelarue44@gmail.com>
2026-08-30 16:59:52 +0200

    INITIAL VERSION

---
+++ ip.py
@@ -0,0 +1,62 @@
+import socket
+from functools import lru_cache
+
+
+def get_local_ipv4() -> str:
+    """Récupère l'adresse IP locale de la machine hôte."""
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+        s.close()
+        return local_ip
+    except Exception:
+        return "127.0.0.1"
+
+
+def get_local_ipv6() -> str:
+    """Récupère l'adresse IPv6 locale principale."""
+    try:
+        s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        # Connexion fictive vers un serveur DNS IPv6 public
+        s.connect(("2001:4860:4860::8888", 80))
+        local_ipv6 = s.getsockname()[0]
+        s.close()
+        return local_ipv6
+    except Exception:
+        return ""
+
+
+@lru_cache(maxsize=1024)
+def resolve_ip(ip: str) -> str:
+    """
+    Traduit une adresse IP en nom de domaine.
+    Le cache @lru_cache évite d'interroger le réseau pour des IP déjà vues.
+    """
+    # Bypass the special IPs
+    if ip.startswith(("127.", "192.168.", "10.", "Inconnu")):
+        return ip
+
+    try:
+        socket.setdefaulttimeout(0.5)
+        nom_hote = socket.gethostbyaddr(ip)[0]
+
+        # Shorten the domain name
+        if len(nom_hote) > 30:
+            nom_hote = nom_hote[:27] + "..."
+
+        return nom_hote
+    except Exception:
+        # If the resolving of IP fail
+        return ip
+
+
+LOCAL_IPV4 = get_local_ipv4()
+LOCAL_IPV6 = get_local_ipv6()
+
+
+def is_my_ip(ip_raw: str) -> bool:
+    """Vérifie si une adresse (IPv4 ou IPv6) appartient à la machine hôte."""
+    if not ip_raw:
+        return False
+    return bool(ip_raw == LOCAL_IPV4 or (LOCAL_IPV6 and ip_raw == LOCAL_IPV6))
---
+++ main.py
@@ -0,0 +1,223 @@
+import argparse
+import time
+from collections import Counter, deque
+
+import keyboard
+from rich.console import Console
+from rich.layout import Layout
+from rich.live import Live
+from rich.panel import Panel
+from rich.table import Table
+from scapy.layers.inet import ICMP, IP, TCP, UDP
+from scapy.layers.inet6 import IPv6
+from scapy.sendrecv import AsyncSniffer
+
+from ip import is_my_ip, resolve_ip
+
+
+def validate_port(value):
+    """Verify that the port is in between 1 and 65535."""
+    try:
+        port = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"'{value}' is not an integer")
+
+    if not (1 <= port <= 65535):
+        raise argparse.ArgumentTypeError(
+            f"The port {port} is invalid. It needs to be between 1 and 65535."
+        )
+
+    return port
+
+
+def analyse_arguments():
+    """Check the command line arguments for the main program"""
+    parser = argparse.ArgumentParser(description="Network Analyser CLI - Project CS50x")
+    parser.add_argument(
+        "-p", "--protocol", choices=["tcp", "udp", "icmp"], help="Filter by protocol"
+    )
+    parser.add_argument(
+        "--port",
+        type=validate_port,
+        help="Filter by source or destination port (ex: 443, 80)",
+    )
+    parser.add_argument(
+        "--ip", type=str, help="Filter by IP address (source or destination)"
+    )
+    return parser.parse_args()
+
+
+def build_filter_bpf(args):
+    """Translate user arguments into BPF filter for Scapy."""
+    conditions = []
+    if args.protocol:
+        conditions.append(args.protocol.lower())
+    if args.port:
+        conditions.append(f"port {args.port}")
+    if args.ip:
+        conditions.append(f"host {args.ip}")
+
+    # Assemble the condition for the bfp
+    bpf_filter = " and ".join(conditions) if conditions else None
+    return bpf_filter
+
+
+stats = {"total": 0, "tcp": 0, "udp": 0, "icmp": 0, "autres": 0}
+derniers_paquets = deque(maxlen=15)
+ip_counter = Counter()
+
+
+def analyse_paquet(paquet):
+    """Analyse a paquet to retrieve its IP address & resolve it"""
+    stats["total"] += 1
+
+    protocole = "OTHER"
+    couleur = "white"
+
+    if paquet.haslayer(TCP):
+        protocole = "TCP"
+        couleur = "green"
+        stats["tcp"] += 1
+    elif paquet.haslayer(UDP):
+        protocole = "UDP"
+        couleur = "cyan"
+        stats["udp"] += 1
+    elif paquet.haslayer(ICMP):
+        protocole = "ICMP"
+        couleur = "yellow"
+        stats["icmp"] += 1
+    else:
+        stats["autres"] += 1
+
+    raw_src, raw_dst = "", ""
+
+    if paquet.haslayer(IP):
+        raw_src = paquet[IP].src
+        raw_dst = paquet[IP].dst
+    elif paquet.haslayer(IPv6):
+        raw_src = paquet[IPv6].src
+        raw_dst = paquet[IPv6].dst
+
+    if raw_src or raw_dst:
+        # Résolution du nom de domaine/hôte
+        ip_src = resolve_ip(raw_src)
+        ip_dst = resolve_ip(raw_dst)
+
+        key_src = "You" if is_my_ip(raw_src) else ip_src
+        key_dst = "You" if is_my_ip(raw_dst) else ip_dst
+
+        if raw_src:
+            ip_counter[key_src] += 1
+        if raw_dst:
+            ip_counter[key_dst] += 1
+
+        if is_my_ip(raw_src):
+            ip_src = f"[bold cyan]YOU[/bold cyan] ({ip_src})"
+        if is_my_ip(raw_dst):
+            ip_dst = f"[bold cyan]YOU[/bold cyan] ({ip_dst})"
+    else:
+        ip_src, ip_dst = "Non-IP", "Non-IP"
+
+    taille = len(paquet)
+
+    derniers_paquets.append(
+        (f"[{couleur}]{protocole}[/{couleur}]", ip_src, ip_dst, f"{taille} octets")
+    )
+
+
+def generateCLI(en_pause=False, filtre_actif="Aucun") -> Layout:
+    """Generate the function interface thanks to the RICH library"""
+
+    layout = Layout()
+
+    layout.split_column(Layout(name="header", size=6), Layout(name="body"))
+
+    layout["body"].split_row(
+        Layout(name="journal", ratio=7), Layout(name="top_ips", ratio=3)
+    )
+
+    # Header
+    titre_status = (
+        "[bold red] [PAUSE] [/bold red]"
+        if en_pause
+        else "[bold green] [LIVE] [/bold green]"
+    )
+    titre_panel = f"PyPacket - Statistics {titre_status} | Active filter : [yellow]{filtre_actif}[/yellow]"
+
+    texte_stats = (
+        f"[bold]Total recorded:[/bold] {stats['total']} paquets\n"
+        f"[green]TCP:[/green] {stats['tcp']} | "
+        f"[cyan]UDP:[/cyan] {stats['udp']} | "
+        f"[yellow]ICMP:[/yellow] {stats['icmp']} | "
+        f"Others: {stats['autres']}\n"
+        f"[dim]Astuce : Press SPACE to toggle pause/resume[/dim]"
+    )
+    layout["header"].update(Panel(texte_stats, title=titre_panel))
+
+    # Traffic logs
+    tableau_journal = Table(expand=True, title="Traffic log(15 derniers paquets)")
+    tableau_journal.add_column("Protocol", justify="center", style="bold")
+    tableau_journal.add_column("Source IP", justify="center")
+    tableau_journal.add_column("Destination IP", justify="center")
+    tableau_journal.add_column("Size", justify="right")
+
+    for pkt in reversed(derniers_paquets):
+        tableau_journal.add_row(pkt[0], pkt[1], pkt[2], pkt[3])
+
+    layout["journal"].update(
+        Panel(tableau_journal, border_style="red" if en_pause else "blue")
+    )
+
+    # Top 5 of IP address
+    tableau_top = Table(expand=True, title="Top 5 - IP Address")
+    tableau_top.add_column("Adress / Host", justify="left", style="yellow")
+    tableau_top.add_column("Paquets", justify="right", style="bold magenta")
+
+    for ip, count in ip_counter.most_common(5):
+        tableau_top.add_row(str(ip), str(count))
+
+    layout["top_ips"].update(Panel(tableau_top, border_style="yellow"))
+
+    return layout
+
+
+if __name__ == "__main__":
+    args = analyse_arguments()
+    bpf_filtre = build_filter_bpf(args)
+
+    console = Console()
+    console.clear()
+
+    sniffer = AsyncSniffer(prn=analyse_paquet, filter=bpf_filtre, store=False)
+    sniffer.start()
+
+    en_pause = False
+    description_filtre = bpf_filtre if bpf_filtre else "None (Capture All)"
+
+    try:
+        with Live(
+            generateCLI(
+                en_pause,
+                description_filtre,
+            ),
+            refresh_per_second=10,
+            screen=True,
+        ) as live:
+            while True:
+                if keyboard.is_pressed("space"):
+                    en_pause = not en_pause
+                    live.update(generateCLI(en_pause, description_filtre))
+                    time.sleep(0.3)
+
+                if not en_pause:
+                    live.update(generateCLI(en_pause, description_filtre))
+
+                time.sleep(0.1)
+
+    except KeyboardInterrupt:
+        if sniffer.running:
+            sniffer.stop()
+        console.print("\n[bold red]Recording stopped by the user[/bold red]")
+        console.print(
+            f"Final bilan : {stats['total']} paquets analysed with the filter : {description_filtre}"
+        )
